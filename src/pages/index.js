d243e064fb831a5bfb4f60241bc6231f0558ace6
Thiago Marinho <tgmarinho@gmail.com>
2019-08-27 14:27:43 -0400

    organizing project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Meu blog</h1>
-    <p>nosa que rapido.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>home</h1>
   </Layout>
 )
 
