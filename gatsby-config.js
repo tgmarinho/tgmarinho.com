a12701e1b7d617aef7516db9ef191b7be19dca2e
Thiago Marinho <tgmarinho@gmail.com>
2019-08-28 09:43:45 -0400

    installing styled components and plugin gatsby fro styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@my-blog-gatsby`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
