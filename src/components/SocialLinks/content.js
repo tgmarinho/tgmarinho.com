1aab9a8052bd50ab8eb3ad1ffdec80b65565c150
Thiago Marinho <tgmarinho@gmail.com>
2019-08-28 10:59:58 -0400

    creating SocialLinks components part 1

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/tgmarinho",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/tgmarinho",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/tgmarinho",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/tgmarinho",
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com/tgmarinho",
+  },
+]
+
+export default links
