0570c0fe2dce642603c002f98f9c55d6c13af5bc
Thiago Marinho <tgmarinho@gmail.com>
2019-08-30 15:26:21 -0400

    creating page of Search and Component Search

---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import Search from "../components/Search"
+
+const SearchPage = () => (
+  <Layout>
+    <SEO title="Search" />
+    <Search />
+  </Layout>
+)
+
+export default SearchPage
