a4beb0ea29dfd4e37e74be5c6866aad139300f14
Thiago Marinho <tgmarinho@gmail.com>
2019-08-31 16:06:25 -0400

    Applying transictions for the pages

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -27,12 +27,26 @@ const MenuBar = () => {
   return (
     <S.MenuBarWrapper>
       <S.MenuBarGroup>
-        <S.MenuBarLink to="/" title="Voltar para Home">
+        <S.MenuBarLink
+          to="/"
+          title="Voltar para Home"
+          cover
+          direction="right"
+          bg="#16202c"
+          duration={0.6}
+        >
           <S.MenuBarItem>
             <Home />
           </S.MenuBarItem>
         </S.MenuBarLink>
-        <S.MenuBarLink to="/search/" title="Pesquisar">
+        <S.MenuBarLink
+          to="/search/"
+          title="Pesquisar"
+          cover
+          direction="right"
+          bg="#16202c"
+          duration={0.6}
+        >
           <S.MenuBarItem>
             <Search />
           </S.MenuBarItem>
--- src/components/RecommendedPosts/index.js
+++ src/components/RecommendedPosts/index.js
@@ -5,13 +5,27 @@ import * as S from "./styled"
 const RecommendedPosts = ({ next, previous }) => (
   <S.RecommendedWrapper>
     {previous && (
-      <S.RecommendedLink to={previous.fields.slug} className="previous">
+      <S.RecommendedLink
+        to={previous.fields.slug}
+        className="previous"
+        cover
+        direction="left"
+        bg="#16202c"
+        duration={0.6}
+      >
         {previous.frontmatter.title}
       </S.RecommendedLink>
     )}
 
     {next && (
-      <S.RecommendedLink to={next.fields.slug} className="next">
+      <S.RecommendedLink
+        to={next.fields.slug}
+        className="next"
+        cover
+        direction="right"
+        bg="#16202c"
+        duration={0.6}
+      >
         {next.frontmatter.title}
       </S.RecommendedLink>
     )}
