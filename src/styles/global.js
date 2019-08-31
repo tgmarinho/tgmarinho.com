ae3b269dfddb3bdf7be8c3ca302fc18a3d59a108
Thiago Marinho <tgmarinho@gmail.com>
2019-08-31 08:21:00 -0400

    creating custom html for gatsby

--- src/styles/global.js
+++ src/styles/global.js
@@ -73,7 +73,7 @@ const GlobalStyles = createGlobalStyle`
     --white: #fff;
     --black: #222;
   }
-  body {
+  body.light {
     --borders: #dedede;
     --postColor: #111;
     --texts: #555555;
